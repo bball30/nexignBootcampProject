df87c0c8aa62d862f3965b43b9c92d4576918a93
Maksim <maksim@itmo.ru>
2023-04-29 02:34:06 +0300

    README.md done

--- crm/src/main/java/ru/bootcamp/crm/config/WebSecurityConfig.java
+++ crm/src/main/java/ru/bootcamp/crm/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ru.bootcamp.crm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/swagger-ui").permitAll()
+                .antMatchers(HttpMethod.GET, "/v3/api-docs").permitAll()
                 .antMatchers("/brt/getAbonents").not().authenticated()
                 .antMatchers("/abonent/pay").hasRole("USER")
                 .antMatchers("/abonent/report/{numberPhone}")
